f2a1682e2722940175d9dca97db55fe7e7f09988
Igor Dianov <igor@dianov.com>
2018-10-16 16:51:01 -0700

    feat: init Stagemonitor metrics reporting

--- src/main/java/org/activiti/cloud/query/qraphql/notifications/GraphqlNotificationsApplication.java
+++ src/main/java/org/activiti/cloud/query/qraphql/notifications/GraphqlNotificationsApplication.java
@@ -3,12 +3,15 @@ package org.activiti.cloud.query.qraphql.notifications;
 import org.activiti.cloud.services.query.notifications.config.EnableActivitiNotificationsGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.stagemonitor.core.Stagemonitor;
 
 @SpringBootApplication
 @EnableActivitiNotificationsGateway
 public class GraphqlNotificationsApplication {
 
 	public static void main(String[] args) {
+        Stagemonitor.init();
+		
 		SpringApplication.run(GraphqlNotificationsApplication.class, args);
 	}
 }
