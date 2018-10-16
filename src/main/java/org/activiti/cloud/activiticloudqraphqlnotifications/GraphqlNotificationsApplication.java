a2fe35971ae90c31b9fda9b246550b3f2ac1b37a
Igor Dianov <igor@dianov.com>
2018-10-16 13:17:43 -0700

    feat: enable activiti-cloud-services-query-graphql-notifications app

--- src/main/java/org/activiti/cloud/activiticloudqraphqlnotifications/GraphqlNotificationsApplication.java
+++ src/main/java/org/activiti/cloud/activiticloudqraphqlnotifications/GraphqlNotificationsApplication.java
@@ -1,12 +1,14 @@
 package org.activiti.cloud.activiticloudqraphqlnotifications;
 
+import org.activiti.cloud.services.query.notifications.config.EnableActivitiNotificationsGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableActivitiNotificationsGateway
+public class GraphqlNotificationsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GraphqlNotificationsApplication.class, args);
 	}
 }
--- src/test/java/org/activiti/cloud/activiticloudqraphqlnotifications/GraphqlNotificationsApplicationTests.java
+++ src/test/java/org/activiti/cloud/activiticloudqraphqlnotifications/GraphqlNotificationsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class GraphqlNotificationsApplicationTests {
 
 	@Test
 	public void contextLoads() {
