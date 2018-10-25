e4bd84b97eac42a8b478764611d3655d54ec1518
Igor Dianov <igor@dianov.com>
2018-10-24 22:21:52 -0700

    feat: add activiti-cloud-services-stagemonitor 7.0.0-SNAPSHOT

--- src/main/java/org/activiti/cloud/query/graphql/notifications/GraphqlNotificationsApplication.java
+++ src/main/java/org/activiti/cloud/query/graphql/notifications/GraphqlNotificationsApplication.java
@@ -2,14 +2,11 @@ package org.activiti.cloud.query.graphql.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.stagemonitor.core.Stagemonitor;
 
 @SpringBootApplication
 public class GraphqlNotificationsApplication {
 
 	public static void main(String[] args) {
-        Stagemonitor.init();
-		
 		SpringApplication.run(GraphqlNotificationsApplication.class, args);
 	}
 }
